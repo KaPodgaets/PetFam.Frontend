d9821537090b3809662d5c71ecf668324bc8f67e
Podgaets Konstantin <kapodgaets@gmail.com>
2024-11-28 11:26:35 +0200

    add errorPage for any undefined path

--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -6,15 +6,17 @@ import TicTacToe from "./pages/GamePage/TicTacToe.tsx";
 import About from "./pages/About.tsx";
 import GameFromTutorialPage from "./pages/GameFromTutorialPage.tsx";
 import HomePage from "./pages/Index/HomePage.tsx";
+import ErrorPage from "./pages/ErrorPage/ErrorPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
-      <Route element={<MainLayout />}>
+      <Route element={<MainLayout />} errorElement={<ErrorPage />}>
         <Route path="/" element={<HomePage />} />
         <Route path="game" element={<TicTacToe />} />
         <Route path="game-from-tutorial" element={<GameFromTutorialPage />} />
         <Route path="about" element={<About />} />
+        <Route path="*" element={<ErrorPage />} />
       </Route>
     </Routes>
   </BrowserRouter>
