ad928c6814ceef9973487b4f4574f943849df830
Kosta P <kapodgaets@gmail.com>
2024-12-19 13:54:58 +0200

    create elements on login page

--- playground/src/pages/Login/LoginPage.tsx
+++ playground/src/pages/Login/LoginPage.tsx
@@ -1,7 +1,26 @@
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+
 export default function LoginPage() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="flex flex-col justify-center items-center my-auto h-full bg-white">
+      <h1 className="text-2xl pb-4">Login</h1>
+      <div className="py-2">
+        <TextField id="outlined-basic" label="Login" variant="outlined" />
+      </div>
+      <div className="py-2">
+        <TextField
+          id="outlined-basic"
+          label="Password"
+          variant="outlined"
+          type="password"
+        />
+      </div>
+      <div className="py-2">
+        <Button variant="contained" size="large">
+          Login
+        </Button>
+      </div>
     </div>
   );
 }
