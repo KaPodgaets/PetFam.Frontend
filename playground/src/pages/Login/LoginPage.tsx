3e7b0e280fa4e86d06608b8e2c8fd0e73ead0899
Kosta P <kapodgaets@gmail.com>
2024-12-19 17:28:10 +0200

    finish tests

--- playground/src/pages/Login/LoginPage.tsx
+++ playground/src/pages/Login/LoginPage.tsx
@@ -3,17 +3,27 @@ import TextField from "@mui/material/TextField";
 import { useEffect, useState } from "react";
 import { getHello } from "../../api/hello";
 import CircularProgress from "@mui/material/CircularProgress";
+import { ToastContainer, toast } from "react-toastify";
+import { Bounce } from "react-toastify/unstyled";
 
 export default function LoginPage() {
   const [text, setText] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const notify = (x: string) => toast(x);
 
   useEffect(() => {
     const fetchHello = async () => {
       setIsLoading(true);
-      const result: string = await getHello();
-      setText(result);
-      setIsLoading(false);
+      try {
+        const result: string = await getHello();
+        setText(result);
+        setIsLoading(false);
+        notify(result);
+      } catch (error) {
+        setIsLoading(false);
+        notify("wow that's an error");
+        console.error(error);
+      }
     };
 
     fetchHello();
@@ -21,6 +31,21 @@ export default function LoginPage() {
 
   return (
     <div className="flex flex-col justify-center items-center my-auto h-full bg-white">
+      <div>
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+          transition={Bounce}
+        />
+      </div>
       <div>{isLoading ? <CircularProgress /> : <p>{text}</p>}</div>
       <h1 className="text-2xl pb-4">Login</h1>
       <div className="py-2">
